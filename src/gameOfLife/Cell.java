64d7e0acbb5061464db0a957c80d6ed9b6c7f145
ArthurTemporim <arthurrtl@gmail.com>
2016-11-20 21:08:28 -0200

    Add agent to project.

--- src/gameOfLife/Cell.java
+++ src/gameOfLife/Cell.java
@@ -1,5 +1,11 @@
 package gameOfLife;
 
-public class Cell {
+import jade.core.Agent;
 
+public class Cell extends Agent{
+
+	private static final long serialVersionUID = 1L;
+
+	protected void setup() {
+	}
 }
--- src/gameOfLife/Main.java
+++ src/gameOfLife/Main.java
@@ -4,15 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		Game g = new Game(10,10);
-		//b.printBoard();
-    //System.out.println();
-    g.initializeBoard();
+		g.initializeBoard();
 		g.printBoard();
-    System.out.println(g.isAlive(0,0));
-    g.insertCell(0,0,true);
-    g.printBoard();
-    System.out.println(g.isAlive(0,0));
-    System.out.println(g.checkNeighbors(1,1));
-    System.out.println(g.checkNeighbors(3,3));
+		System.out.println(g.isAlive(0,0));
+		g.insertCell(0,0,true);
+		g.printBoard();
+		System.out.println(g.isAlive(0,0));
+		System.out.println(g.checkNeighbors(1,1));
+		System.out.println(g.checkNeighbors(3,3));
 	}
 }
