28c671603eefe577f31c7e368bfcedb25e37a21c
ArthurTemporim <arthurrtl@gmail.com>
2016-12-04 10:44:36 -0200

    Correct a mistake in initialization of Cell matrix.

--- src/gameOfLife/Cell.java
+++ src/gameOfLife/Cell.java
@@ -35,8 +35,14 @@ public class Cell extends Agent{
 		Cell[][] board = null;
 		int width = 9;
 		int height = 9;
+		
 		board = initializeBoard(board, width, height);
 		printBoard(board, width, height);
+		neighborsAlive(board,1,1);
+		
+		board[2][2].setAlive(true);
+		printBoard(board, width, height);
+		neighborsAlive(board,1,1);
 		
 		try {
 			
@@ -48,62 +54,62 @@ public class Cell extends Agent{
 // ----------------------------------------------------------------------------
 	
 	// Return the amount of neighbors alive.	
-	public int neighborsAlive(Cell cell) {
+	public int neighborsAlive(Cell[][] board, int x, int y) {
 		int amountOfNeighborsAlive = 0;
 		
 		// Get the position of current cell.
-		int cellX = cell.getX();
-		int cellY = cell.getY();
+		int cellX = board[x][y].getX();
+		int cellY = board[x][y].getY();
 		
 		// Cell aux to check if the neighbor is alive.
-		Cell neighbor;
+		Cell neighbor = null;
 		
-		// Up, left neighbor.	
-		neighbor = new Cell(cellX-1, cellY-1);
+		// Up, left neighbor.
+		neighbor = board[cellX-1][cellY-1];
 		if(neighbor.getAlive()) {
 			amountOfNeighborsAlive++;
 		}
 		// Up neighbor.
-		neighbor = new Cell(cellX, cellY-1);
+		neighbor = board[cellX][cellY-1];
 		if(neighbor.getAlive()) {
 			amountOfNeighborsAlive++;
 		}
 		// Up, right neighbor.
-		neighbor = new Cell(cellX+1, cellY-1);
+		neighbor = board[cellX+1][cellY-1];
 		if(neighbor.getAlive()) {
 			amountOfNeighborsAlive++;
 		}
 		
 		// Left neighbor.
-		neighbor = new Cell(cellX-1, cellY);
+		neighbor = board[cellX-1][cellY];
 		if(neighbor.getAlive()) {
 			amountOfNeighborsAlive++;
 		}
 		// Right neighbor.
-		neighbor = new Cell(cellX+1, cellY);
+		neighbor = board[cellX+1][cellY];
 		if(neighbor.getAlive()) {
 			amountOfNeighborsAlive++;
 		}
 		
 		// Down, left neighbor.	
-		neighbor = new Cell(cellX-1, cellY+1);
+		neighbor = board[cellX-1][cellY+1];
 		if(neighbor.getAlive()) {
 			amountOfNeighborsAlive++;
 		}
 		// Down neighbor.
-		neighbor = new Cell(cellX, cellY+1);
+		neighbor = board[cellX][cellY+1];
 		if(neighbor.getAlive()) {
 			amountOfNeighborsAlive++;
 		}
 		// Down, right neighbor.
-		neighbor = new Cell(cellX+1, cellY+1);
+		neighbor = board[cellX+1][cellY+1];
 		if(neighbor.getAlive()) {
 			amountOfNeighborsAlive++;
 		}
 		
 		// Verification.
 		System.out.println("Neighbors alive of cell: " 
-		+ cell.getX() + " " + getY() + " = " + amountOfNeighborsAlive);
+		+ board[x][y].getX() + "," + board[x][y].getX() + " = " + amountOfNeighborsAlive);
 		
 		return amountOfNeighborsAlive;
 	}
@@ -113,12 +119,13 @@ public class Cell extends Agent{
 		board = new Cell[width][height];
 		for(int i=0;i<width;i++) {
 			for(int j=0;j<height;j++) {
-				board[i][j] = new Cell();
+				board[i][j] = new Cell(i,j);
 			}
 		}
 		return board;
 	}
 	
+	// Print the matrix of cells.
 	public void printBoard(Cell[][] board, int width, int height) {
 		for(int i=0;i<width;i++) {
 			for(int j=0;j<height;j++) {
